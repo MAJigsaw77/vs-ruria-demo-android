01b265ec0bca87aec60049629f14b72ce21ebc26
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-13 20:21:08 +0200

    Initial commit

---
+++ source/MusicBeatState.hx
@@ -0,0 +1,144 @@
+package;
+
+import Conductor.BPMChangeEvent;
+import flixel.FlxG;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.ui.FlxUIState;
+import flixel.math.FlxRect;
+import flixel.util.FlxTimer;
+import Options;
+#if android
+import flixel.input.actions.FlxActionInput;
+import android.AndroidControls.AndroidControls;
+import android.FlxVirtualPad;
+#end
+
+class MusicBeatState extends FlxUIState
+{
+	private var lastBeat:Float = 0;
+	private var lastStep:Float = 0;
+
+	private var curStep:Int = 0;
+	private var curBeat:Int = 0;
+	private var controls(get, never):Controls;
+
+	inline function get_controls():Controls
+		return PlayerSettings.player1.controls;
+
+	#if android
+	var _virtualpad:FlxVirtualPad;
+	var androidc:AndroidControls;
+	var trackedinputs:Array<FlxActionInput> = [];
+	#end
+	
+	#if android
+	public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
+		_virtualpad = new FlxVirtualPad(DPad, Action);
+		_virtualpad.alpha = 0.75;
+		add(_virtualpad);
+		controls.setVirtualPad(_virtualpad, DPad, Action);
+		trackedinputs = controls.trackedinputs;
+		controls.trackedinputs = [];
+	}
+	#end
+
+	#if android
+	public function addAndroidControls() {
+                androidc = new AndroidControls();
+
+		switch (androidc.mode)
+		{
+			case VIRTUALPAD_RIGHT | VIRTUALPAD_LEFT | VIRTUALPAD_CUSTOM:
+				controls.setVirtualPad(androidc._virtualPad, FULL, NONE);
+			case DUO:
+				controls.setVirtualPad(androidc._virtualPad, DUO, NONE);
+			case HITBOX:
+				controls.setHitBox(androidc._hitbox);
+			default:
+		}
+
+		trackedinputs = controls.trackedinputs;
+		controls.trackedinputs = [];
+
+		var camcontrol = new flixel.FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		androidc.cameras = [camcontrol];
+
+		androidc.visible = false;
+
+		add(androidc);
+	}
+	#end
+
+	#if android
+        public function addPadCamera() {
+		var camcontrol = new flixel.FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		_virtualpad.cameras = [camcontrol];
+	}
+	#end
+	
+	override function destroy() {
+		#if android
+		controls.removeFlxInput(trackedinputs);
+		#end	
+		
+		super.destroy();
+	}
+
+	override function create()
+	{
+		if (transIn != null)
+			trace('reg ' + transIn.region);
+
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		//everyStep();
+		var oldStep:Int = curStep;
+
+		updateCurStep();
+		updateBeat();
+
+		if (oldStep != curStep && curStep > 0)
+			stepHit();
+
+		super.update(elapsed);
+	}
+
+	private function updateBeat():Void
+	{
+		curBeat = Math.floor(curStep / 4);
+	}
+
+	private function updateCurStep():Void
+	{
+		var lastChange:BPMChangeEvent = {
+			stepTime: 0,
+			songTime: 0,
+			bpm: 0
+		}
+		for (i in 0...Conductor.bpmChangeMap.length)
+		{
+			if (Conductor.songPosition - OptionUtils.options.noteOffset >= Conductor.bpmChangeMap[i].songTime)
+				lastChange = Conductor.bpmChangeMap[i];
+		}
+
+		curStep = lastChange.stepTime + Math.floor((Conductor.songPosition - OptionUtils.options.noteOffset - lastChange.songTime) / Conductor.stepCrochet);
+	}
+
+	public function stepHit():Void
+	{
+		if (curStep % 4 == 0)
+			beatHit();
+	}
+
+	public function beatHit():Void
+	{
+		//do literally nothing dumbass
+	}
+}
---
+++ source/Note.hx
@@ -0,0 +1,249 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.math.FlxMath;
+import flixel.util.FlxColor;
+
+using StringTools;
+
+class Note extends FlxSprite
+{
+	public var strumTime:Float = 0;
+
+	public var mustPress:Bool = false;
+	public var noteData:Int = 0;
+	public var canBeHit:Bool = false;
+	public var tooLate:Bool = false;
+	public var wasGoodHit:Bool = false;
+	public var prevNote:Note;
+	public var hit:Bool = false;
+	public var rating:String = "sick";
+	public var lastSustainPiece = false;
+	public var defaultX:Float = 0;
+	public var sustainLength:Float = 0;
+	public var isSustainNote:Bool = false;
+	public var gfSings:Bool = false; // TODO: replace w/ a player var? 1 for bf, 2 for opponent and 3 for gf
+	public var rawNoteData:Int = 0; // for charting shit and thats it LOL
+	public var noteType:Float = 0;
+	public var beingCharted=false;
+	public var noteScore:Float = 1;
+	public var holdParent:Bool=false;
+
+	public static var swagWidth:Float = 160 * 0.7;
+	public static var PURP_NOTE:Int = 0;
+	public static var GREEN_NOTE:Int = 2;
+	public static var BLUE_NOTE:Int = 1;
+	public static var RED_NOTE:Int = 3;
+
+	public function new(strumTime:Float, noteData:Int, ?prevNote:Note, ?sustainNote:Bool = false, ?noteType:Float=0)
+	{
+		super();
+
+		if (prevNote == null)
+			prevNote = this;
+
+		this.noteType=noteType;
+		this.prevNote = prevNote;
+		isSustainNote = sustainNote;
+
+		x += 50;
+		// MAKE SURE ITS DEFINITELY OFF SCREEN?
+		y -= 2000;
+		this.strumTime = strumTime;
+
+		this.noteData = noteData;
+
+		var daStage:String = PlayState.curStage;
+
+		if(noteType==0 || isSustainNote){
+			switch (daStage)
+			{
+				case 'school' | 'schoolEvil':
+					loadGraphic(Paths.image('weeb/pixelUI/arrows-pixels'), true, 17, 17);
+
+					animation.add('greenScroll', [6]);
+					animation.add('redScroll', [7]);
+					animation.add('blueScroll', [5]);
+					animation.add('purpleScroll', [4]);
+
+					if (isSustainNote)
+					{
+						loadGraphic(Paths.image('weeb/pixelUI/arrowEnds'), true, 7, 6);
+
+						animation.add('purpleholdend', [4]);
+						animation.add('greenholdend', [6]);
+						animation.add('redholdend', [7]);
+						animation.add('blueholdend', [5]);
+
+						animation.add('purplehold', [0]);
+						animation.add('greenhold', [2]);
+						animation.add('redhold', [3]);
+						animation.add('bluehold', [1]);
+					}
+
+					setGraphicSize(Std.int(width * PlayState.daPixelZoom));
+					updateHitbox();
+
+				default:
+					frames = Paths.getSparrowAtlas('NOTE_assets');
+
+					animation.addByPrefix('greenScroll', 'green0');
+					animation.addByPrefix('redScroll', 'red0');
+					animation.addByPrefix('blueScroll', 'blue0');
+					animation.addByPrefix('purpleScroll', 'purple0');
+
+					animation.addByPrefix('purpleholdend', 'pruple end hold');
+					animation.addByPrefix('greenholdend', 'green hold end');
+					animation.addByPrefix('redholdend', 'red hold end');
+					animation.addByPrefix('blueholdend', 'blue hold end');
+
+					animation.addByPrefix('purplehold', 'purple hold piece');
+					animation.addByPrefix('greenhold', 'green hold piece');
+					animation.addByPrefix('redhold', 'red hold piece');
+					animation.addByPrefix('bluehold', 'blue hold piece');
+
+					setGraphicSize(Std.int(width * 0.7));
+					updateHitbox();
+					antialiasing = true;
+			}
+		}else if(noteType==1){
+			frames = Paths.getSparrowAtlas('HurtNotes');
+			animation.addByPrefix('greenScroll', 'UPFake');
+			animation.addByPrefix('redScroll', 'RIGHTFake');
+			animation.addByPrefix('blueScroll', 'DOWNFake');
+			animation.addByPrefix('purpleScroll', 'LEFTFake');
+
+			setGraphicSize(Std.int(width * 0.7));
+			updateHitbox();
+			antialiasing = true;
+		}else if(noteType==2){
+			frames = Paths.getSparrowAtlas('KillNotes');
+			animation.addByPrefix('greenScroll', 'UPKill');
+			animation.addByPrefix('redScroll', 'RIGHTKill');
+			animation.addByPrefix('blueScroll', 'DOWNKill');
+			animation.addByPrefix('purpleScroll', 'LEFTKill');
+
+			setGraphicSize(Std.int(width * 0.7));
+			updateHitbox();
+			antialiasing = true;
+		}
+
+		switch (noteData)
+		{
+			case 0:
+				x += swagWidth * 0;
+				animation.play('purpleScroll');
+			case 1:
+				x += swagWidth * 1;
+				animation.play('blueScroll');
+			case 2:
+				x += swagWidth * 2;
+				animation.play('greenScroll');
+			case 3:
+				x += swagWidth * 3;
+				animation.play('redScroll');
+		}
+
+		// trace(prevNote);
+
+		if (isSustainNote && prevNote != null)
+		{
+			prevNote.holdParent=true;
+			noteScore * 0.2;
+			alpha = 0.6;
+
+			//var off = -width;
+			var off = -width/4;
+			//x+=width/2;
+
+			switch (noteData)
+			{
+				case 2:
+					animation.play('greenholdend');
+				case 3:
+					animation.play('redholdend');
+				case 1:
+					animation.play('blueholdend');
+				case 0:
+					animation.play('purpleholdend');
+			}
+
+			updateHitbox();
+
+			if(PlayState.currentPState.currentOptions.downScroll){
+				flipY=true;
+			}
+
+			//off -= width / 2;
+			//x -= width / 2;
+			if (PlayState.curStage.startsWith('school'))
+				off -= 36.5;
+			else
+				off -= 2;
+
+
+
+			offset.x = off;
+
+			if (prevNote.isSustainNote)
+			{
+				var offset = prevNote.offset.x;
+				switch (prevNote.noteData)
+				{
+					case 0:
+						prevNote.animation.play('purplehold');
+					case 1:
+						prevNote.animation.play('bluehold');
+					case 2:
+						prevNote.animation.play('greenhold');
+					case 3:
+						prevNote.animation.play('redhold');
+				}
+
+				if(!prevNote.beingCharted){
+					prevNote.scale.y = Conductor.stepCrochet / 100 * prevNote.scale.y * 1.5 * FlxMath.roundDecimal(PlayState.SONG.speed,2);
+					prevNote.updateHitbox();
+				}
+
+				prevNote.offset.x = offset;
+				// prevNote.setGraphicSize();
+			}
+		}
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		if(!beingCharted){
+			if(gfSings)
+				visible=false;
+
+			if (mustPress)
+			{
+				// The * 0.5 is so that it's easier to hit them too late, instead of too early
+					if (strumTime > Conductor.songPosition - (Conductor.safeZoneOffset * 1.5)
+						&& strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * 1))
+						canBeHit = true;
+					else
+						canBeHit = false;
+
+
+
+				if (strumTime < Conductor.songPosition - Conductor.safeZoneOffset && !wasGoodHit)
+					tooLate = true;
+			}
+			else
+			{
+				if (strumTime <= Conductor.songPosition)
+					canBeHit = true;
+			}
+
+			if (tooLate)
+			{
+				if (alpha > 0.3)
+					alpha = 0.3;
+			}
+		}
+	}
+}
